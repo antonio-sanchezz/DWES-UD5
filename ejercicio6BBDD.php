6fa852098fb49caccf4918f3a16494e53906388f
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-11-10 12:06:04 +0100

    6

--- ejercicio6BBDD.php
+++ ejercicio6BBDD.php
@@ -8,32 +8,41 @@ try {
     echo "Connection fallida: " . $e->getMessage();
 }
 
+// Obtenemos todos los datos del usuario solicitado.
 function getUser($username) {
     try {
 
         $sqlQuery = "SELECT * FROM users WHERE username = ?";
         $stmt = $GLOBALS['conn']->prepare($sqlQuery);
         $stmt->bindParam(1, $username);
-    
+
         $stmt->execute();
-    
+
         $user = $stmt->fetch();
 
     } catch (PDOException $e) {
+        $e->getMessage();
     }
 
-    $conn = null;
+    $stmt = null;
 
     return $user;
 }
 
+/*
+* Verificamos que la contraseña introducida es la misma que la de la base de datos.
+*/
 function passwordVerify($username, $password) {
-
+    // Obtenemos el usuario solicitado.
     $user = getUser($username);
+    $result = false;
 
-    $result = password_verify($password, $user['password']);
+    // Comprobamos si hemos conseguido obtener un usuario con los datos dados.
+    if ($user) {
+        // Y ya realizamos la verificacion de la contraseña.
+        $result = password_verify($password, $user['password']);
+    }
+
 
     return $result;
 }
-
-?>
